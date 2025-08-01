db0094488604f206f52095a73955d10f9012a76e
fake js dev <112frosta@proton.me>
2025-08-01 16:00:44 +0200

    Update logging to use Console utility for better formatting

--- src/templates/default/src/index.ts
+++ src/templates/default/src/index.ts
@@ -1,7 +1,8 @@
+import { Console } from "@/lib/utils";
 import { client } from "@lib/client";
 
 client.on("ready", () => {
-  console.log(`Logged in as ${client.user?.tag}!`);
+  Console.Log(`🤖 Logged in as ${client.user?.tag}`);
 });
 
 client.login(process.env.BOT_TOKEN);
