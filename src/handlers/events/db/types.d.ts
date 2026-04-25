1aafddfbc4b1a2a6fcb7b10d5c20da0b54782a1a
fakejsdev <112frosta@proton.me>
2026-04-25 15:27:28 +0200

    Refactor DbEvent types to improve type safety and consistency in event configuration

--- src/handlers/events/db/types.d.ts
+++ src/handlers/events/db/types.d.ts
@@ -1,13 +1,26 @@
+import type { Prisma } from "@lib/prisma/generated/client";
+
 declare global {
-	type DbEventConfig = {
+	type DbAction = "Create" | "Update" | "Delete";
+
+	type DbEventName = `${Prisma.ModelName}.${DbAction}`;
+
+	type PayloadType<T extends string> = T extends Prisma.ModelName
+		? Prisma.TypeMap["model"][T]["payload"]["scalars"]
+		: never;
+
+	type DbEventConfig<T extends DbEventName = DbEventName> = {
 		name: string;
+		on: T;
 		description?: string;
 	};
 
-	type DbEventRun = () => Promise<void> | void;
+	type DbEventRun<T extends DbEventName = DbEventName> = (
+		data: T extends `${infer Model}.${DbAction}` ? PayloadType<Model> : unknown,
+	) => Promise<void> | void;
 
-	type DbEventConfigWithRun = {
-		config: DbEventConfig;
-		run: DbEventRun;
+	type DbEventConfigWithRun<T extends DbEventName = DbEventName> = {
+		config: DbEventConfig<T>;
+		run: DbEventRun<T>;
 	};
 }
