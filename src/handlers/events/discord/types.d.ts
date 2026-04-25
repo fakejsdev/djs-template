0dcd8474bde9e462607a0c87705c5f6efac07c51
fakejsdev <112frosta@proton.me>
2026-04-25 15:01:35 +0200

    Rename event types to include 'Discord' prefix for clarity and consistency

--- src/handlers/events/discord/types.d.ts
+++ src/handlers/events/discord/types.d.ts
@@ -1,18 +1,18 @@
 import type { ClientEvents } from "discord.js";
 
 declare global {
-	type EventConfig = {
+	type DiscordEventConfig = {
 		name: keyof ClientEvents;
 		once?: boolean;
 		description?: string;
 	};
 
-	type EventRun<T extends keyof ClientEvents> = (
+	type DiscordEventRun<T extends keyof ClientEvents> = (
 		...args: ClientEvents[T]
 	) => Promise<void> | void;
 
-	type EventConfigWithRun = {
-		config: EventConfig;
-		run: EventRun<unknown>;
+	type DiscordEventConfigWithRun = {
+		config: DiscordEventConfig;
+		run: DiscordEventRun<unknown>;
 	};
 }
