94bc5880ff4d32f31bbbe812947f5454f1cc8090
fakejsdev <112frosta@proton.me>
2026-04-25 15:54:56 +0200

    Refactor post-create event to improve naming and description for clarity

--- src/modules/example-module/events/db/post-create.ts
+++ src/modules/example-module/events/db/post-create.ts
@@ -3,9 +3,9 @@ import { Console } from "@/lib/utils";
 
 export const { config, run } = createDatabaseEvent(
 	{
-		name: "Example Post Create Event",
+		name: "Post Create Logger",
 		on: "Post.Create",
-		description: "Do smth on post create",
+		description: "Console Logs once a post is created",
 	},
 	async (payload) => {
 		Console.Log("Post created with payload:", payload);
